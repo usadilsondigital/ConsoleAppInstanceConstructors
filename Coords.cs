3a1665db3b1f462cb614ece77403be5b31fc0d00
Adilson <usadilsondigital@gmail.com>
2024-01-30 16:00:20 -0600

    Add project files.

---
+++ Coords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInstanceConstructors
+{
+    internal class Coords
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public Coords(int x , int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppInstanceConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
