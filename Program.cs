7210e185676dec5a4c1b169720ad1f910e5fca48
Adilson <usadilsondigital@gmail.com>
2024-01-30 16:07:21 -0600

    Showing the 2nd Constructor

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@
         {
             var p1 = new Coords();
             Console.WriteLine($"Coords #1 at {p1}");
+
+            var p2 = new Coords(5,3);
+            Console.WriteLine($"Coords #2 at {p2}");
         }
     }
 }
