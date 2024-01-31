140f8ad5abd559eb810f044bbef6349523ae7d14
Adilson <usadilsondigital@gmail.com>
2024-01-31 10:07:59 -0600

    initializing params and calc volume

--- Widget.cs
+++ Widget.cs
@@ -9,8 +9,13 @@ namespace ConsoleAppInstanceConstructors
     public class Widget(string name,int width,int heigth,int depth) : NamedItem(name)
     {
 
+        public int WidthInCM => width;
+        public int HeightInCM => heigth;
+        public int DepthInCM => depth;
+
         public Widget() : this("N/A", 1, 1, 1) { } //unnamed unit cube
 
+        public int Volume => width * heigth * depth;
 
 
     }
