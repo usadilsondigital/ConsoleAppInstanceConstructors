cca196ff34e16e27de5fb2dec54b8ba08582fcee
Adilson <usadilsondigital@gmail.com>
2024-01-31 08:19:06 -0600

    added radius to circle ctor

---
+++ Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInstanceConstructors
+{
+    internal class Circle : Shape
+    {
+        public Circle(double radius) : base(radius, 0)
+        {
+        }
+
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
