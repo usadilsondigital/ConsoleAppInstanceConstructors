ae85e66eb15c754d533f60b992c9e9623cd88d41
Adilson <usadilsondigital@gmail.com>
2024-01-31 09:35:51 -0600

    class nameditem

---
+++ NamedItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInstanceConstructors
+{
+    public class NamedItem(string name)
+    {
+        public string Name => name;
+    }
+
+}
