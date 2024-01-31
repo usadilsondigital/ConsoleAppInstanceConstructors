0753a0f08316629e92af01d70822ba65bde0a3d5
Adilson <usadilsondigital@gmail.com>
2024-01-31 08:07:15 -0600

    adding const pi

---
+++ Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInstanceConstructors
+{
+    abstract  class Shape
+    {
+        public const double pi = Math.PI;
+    }
+}
